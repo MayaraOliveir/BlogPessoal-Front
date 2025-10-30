9c54b1865f021aa3de9b7a965ef352794430309f
MayaraOliveir <mayaraoliveir00@gmail.com>
2025-10-29 21:35:10 -0300

    feat(tema/service): adiciona componentes, formTema e DeletarTema

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import Home from "./pages/home/Home"
-import "./pages/home/Home.css"
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cadastro from "./pages/cadastro/Cadastro";
-import Login from "./pages/login/Login"
-import { AuthProvider } from "./contexts/AuthContext";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import FormTema from "./components/tema/formtema/FormTema";
 import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import { AuthProvider } from "./contexts/AuthContext";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import "./pages/home/Home.css";
+import Login from "./pages/login/Login";
+import DeletarTema from "./components/tema/deletartema/DeletarTema"
+
 
 const App = () => {
   return (
@@ -20,6 +23,10 @@ const App = () => {
             <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema/>}></Route>
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
           </Routes>
         </div>
         <Footer/>
